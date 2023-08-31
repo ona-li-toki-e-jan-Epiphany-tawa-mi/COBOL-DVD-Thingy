8411d89d0b7aff661fcb122c5d67a20b3356bf49
ona-li-toki-e-jan-Epiphany-tawa-mi <darklorddudeal.deaddear@gmail.com>
2023-08-31 04:28:05 -0400

    Swapped to ASCII logo because unicode in COBOL is a pain.
    Changed update interval to every half-second.
    
    Logo length is now read from file.

--- compile.sh
+++ compile.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 
 # TODO Replace this with a make file.
-cobc -x -W -Werror -Wno-unfinished -Wno-terminator dvd_thing.cob
+cobc -x -Wall -Werror -Wno-unfinished dvd_thing.cob
--- dvd_thing.cob
+++ dvd_thing.cob
@@ -2,85 +2,87 @@
        IDENTIFICATION DIVISION.
        PROGRAM-ID. dvd_thing. 
       ******************************************************************
+      * TODO add desciption of program.
+      ******************************************************************
        ENVIRONMENT DIVISION.
        INPUT-OUTPUT SECTION.
        FILE-CONTROL.
-         SELECT F-LOGO-FILE ASSIGN TO "logo.txt"
-            ORGANIZATION IS LINE SEQUENTIAL.
+       SELECT F-LOGO-FILE ASSIGN TO "LOGO.TXT"
+         ORGANIZATION IS LINE SEQUENTIAL.
       ******************************************************************
        DATA DIVISION.
-       FILE SECTION.
-       FD F-LOGO-FILE.
-       01 F-LOGO-IMAGE-LINE PIC N(45).
        WORKING-STORAGE SECTION.
-       01 WS-LOGO-IMAGE.
-          05 WS-LI-LINE PIC N(45) OCCURS 7 TIMES.
        01 WS-EOF PIC X.
-       01 WS-LOGO.
-          05 WS-L-WIDTH   PIC 99 VALUE 30.
-          05 WS-L-HEIGHT  PIC 99 VALUE 7.
-          05 WS-L-X       PIC 99 VALUE 0.
-          05 WS-L-DELTA-X PIC S9 VALUE 1.
-          05 WS-L-Y       PIC 99 VALUE 0.
-          05 WS-L-DELTA-Y PIC S9 VALUE 1.
+       01 WS-I-IDX PIC 999.
+       01 WS-K-IDX PIC 999.
+      * There is the function CBL_GET_SCR_SIZE to get the dimensions of
+      * the terminal, but it makes the terminal misbehave for some 
+      * reason.
        01 WS-TERMINAL.
-      *  TODO See if this can pull terminal size from OS.
-          05 WS-TERMINAL-WIDTH  PIC S99 VALUE 80.
-          05 WS-TERMINAL-HEIGHT PIC S99 VALUE 23.
+          05 WS-T-WIDTH  PIC 999 VALUE 80.
+          05 WS-T-HEIGHT PIC 999 VALUE 23.
        01 WS-FILLER-CHARACTER PIC X VALUE ".".
-       01 WS-I PIC 99.
-       01 WS-K PIC 99.
+       01 WS-LOGO-IMAGE.
+          05 WS-LI-LINE PIC X(31) USAGE NATIONAL OCCURS 8 TIMES.
+       01 WS-LOGO.
+          05 WS-L-WIDTH   PIC 999.
+      * TODO See if this value can be pulled from line image table.
+          05 WS-L-HEIGHT  PIC 999 VALUE 8.
+          05 WS-L-X       PIC 999 VALUE 0.
+          05 WS-L-DELTA-X PIC S9  VALUE 1.
+          05 WS-L-Y       PIC 999 VALUE 0.
+          05 WS-L-DELTA-Y PIC S9  VALUE 1.
       ******************************************************************
        PROCEDURE DIVISION.
        000-MAIN.
-         PERFORM 005-LOAD-LOGO-FROM-FILE.
+         PERFORM 001-LOAD-LOGO-FROM-FILE.
          PERFORM FOREVER
-            PERFORM 005-UPDATE-SCREEN
-            PERFORM 005-MOVE-LOGO
-            CALL "C$SLEEP" USING 1
+            PERFORM 001-UPDATE-TERMINAL
+            PERFORM 001-MOVE-LOGO
+            CALL "CBL_GC_NANOSLEEP" USING 500000000
          END-PERFORM.
-         STOP RUN.
 
-       005-LOAD-LOGO-FROM-FILE.
+       001-LOAD-LOGO-FROM-FILE.
          OPEN INPUT F-LOGO-FILE.
-         MOVE 1 TO WS-I.
-         PERFORM UNTIL (WS-EOF = "Y") OR (WS-I > WS-L-HEIGHT)
-            READ F-LOGO-FILE INTO F-LOGO-IMAGE-LINE 
+         MOVE 1 TO WS-I-IDX.
+         PERFORM UNTIL (WS-EOF = "Y") OR (WS-I-IDX > WS-L-HEIGHT)
+            READ F-LOGO-FILE INTO WS-LI-LINE(WS-I-IDX)
             AT END 
                MOVE 'Y' TO WS-EOF
             NOT AT END
-               MOVE F-LOGO-IMAGE-LINE TO WS-LI-LINE(WS-I)
-               ADD 1 TO WS-I
+               ADD 1 TO WS-I-IDX
          END-PERFORM.
          CLOSE F-LOGO-FILE.
 
-       005-MOVE-LOGO.
+         MOVE FUNCTION LENGTH(WS-LI-LINE(1)) TO WS-L-WIDTH.
+
+       001-MOVE-LOGO.
          ADD WS-L-DELTA-X TO WS-L-X.
          IF (WS-L-X <= 0)
-            OR (WS-L-X >= WS-TERMINAL-WIDTH - WS-L-WIDTH) 
+            OR (WS-L-X >= WS-T-WIDTH - WS-L-WIDTH) 
          THEN
             MULTIPLY -1 BY WS-L-DELTA-X
          END-IF.
 
          ADD WS-L-DELTA-Y TO WS-L-Y.
          IF (WS-L-Y <= 0)
-            OR (WS-L-Y >= WS-TERMINAL-HEIGHT - WS-L-HEIGHT)
+            OR (WS-L-Y >= WS-T-HEIGHT - WS-L-HEIGHT)
          THEN
             MULTIPLY -1 BY WS-L-DELTA-Y
          END-IF.
 
-       005-UPDATE-SCREEN.
+       001-UPDATE-TERMINAL.
          CALL "SYSTEM" USING "clear".
-                  
-         PERFORM VARYING WS-I FROM 1 BY 1 
-                 UNTIL WS-I > WS-TERMINAL-HEIGHT
-            IF (WS-I > WS-L-Y) 
-               AND (WS-I <= WS-L-Y + WS-L-HEIGHT)
+         
+         PERFORM VARYING WS-I-IDX FROM 1 BY 1 
+                 UNTIL WS-I-IDX > WS-T-HEIGHT
+            IF (WS-I-IDX > WS-L-Y) 
+               AND (WS-I-IDX <= WS-L-Y + WS-L-HEIGHT)
             THEN
                PERFORM 010-PRINT-LOGO-LINE
             ELSE
-               PERFORM VARYING WS-K FROM 1 BY 1 
-                       UNTIL WS-K >= WS-TERMINAL-WIDTH
+               PERFORM VARYING WS-K-IDX FROM 1 BY 1 
+                       UNTIL WS-K-IDX >= WS-T-WIDTH
                   DISPLAY WS-FILLER-CHARACTER WITH NO ADVANCING
                END-PERFORM
                DISPLAY "."
@@ -88,31 +90,20 @@
          END-PERFORM.
 
        010-PRINT-LOGO-LINE.
-         PERFORM VARYING WS-K FROM 1 BY 1 UNTIL WS-K > WS-L-X
+         PERFORM VARYING WS-K-IDX FROM 1 BY 1 UNTIL WS-K-IDX > WS-L-X
             DISPLAY WS-FILLER-CHARACTER WITH NO ADVANCING
          END-PERFORM.
 
-         SUBTRACT WS-L-Y FROM WS-I GIVING WS-K.
-         IF WS-L-X + WS-L-WIDTH < WS-TERMINAL-WIDTH
-            DISPLAY WS-LI-LINE(WS-K) WITH NO ADVANCING
+         SUBTRACT WS-L-Y FROM WS-I-IDX GIVING WS-K-IDX.
+         IF WS-L-X + WS-L-WIDTH < WS-T-WIDTH
+            DISPLAY WS-LI-LINE(WS-K-IDX) WITH NO ADVANCING
 
-            ADD WS-L-X, WS-L-WIDTH, 1 GIVING WS-K
-            PERFORM UNTIL WS-K >= WS-TERMINAL-WIDTH
+            ADD WS-L-X, WS-L-WIDTH, 1 GIVING WS-K-IDX
+            PERFORM UNTIL WS-K-IDX >= WS-T-WIDTH
                DISPLAY WS-FILLER-CHARACTER WITH NO ADVANCING
-               ADD 1 TO WS-K
+               ADD 1 TO WS-K-IDX
             END-PERFORM
             DISPLAY WS-FILLER-CHARACTER
          ELSE
-            DISPLAY WS-LI-LINE(WS-K)
+            DISPLAY WS-LI-LINE(WS-K-IDX)
          END-IF.
-
-
-      *      ELSE
-      *         DISPLAY FILLER-CHARACTER WITH NO ADVANCING
-      *      END-IF
-      *   END-PERFORM.
-      *   IF TERMINAL-WIDTH <= LOGO-X + LOGO-WIDTH 
-      *      DISPLAY "A"
-      *   ELSE
-      *      DISPLAY FILLER-CHARACTER
-      *   END-IF.
